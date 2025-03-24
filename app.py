e842656bd61f7d697810578eca7deeaa4d025958
PadalaDevisrisairam <devisri.padala@sasi.ac.in>
2025-03-24 18:16:37 +0530

    version 1

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+# Initialize Flask App
+app = Flask(__name__)
+# Route to Render Registration Form
+@app.route("/")
+def registration_form():
+    return render_template("registration.html")  # Serve the form
+# Run Flask App
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
+  
